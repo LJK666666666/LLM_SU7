8cbaaf0e64559e72809b502b24a04020e53a771b
LJK666666666 <2069927109@qq.com>
2026-01-04 18:12:20 +0800

    fix test mode

--- src/__main__.py
+++ src/__main__.py
@@ -2,8 +2,11 @@
 命令行入口
 使用方法: python -m src --model rf --features base,text
 """
-from .main import parse_args, train
+from .main import parse_args, train, test_only
 
 if __name__ == '__main__':
     args = parse_args()
-    train(args)
+    if args.mode == 'test':
+        test_only(args)
+    else:
+        train(args)
